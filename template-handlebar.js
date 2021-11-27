b062801878c146e7aca86ae38d7e8df6269f5620
surajjayraman <suraj.jayraman@gmail.com>
2021-11-26 20:52:23 -0500

    Add helper function to map obj formats in JSON.

--- template-handlebar.js
+++ template-handlebar.js
@@ -10,3 +10,32 @@ const compiledHtml = newtemplate(context);
 
 const iceCreamText = document.getElementById("scream");
 iceCreamText.innerHTML = compiledHtml;
+
+// Example of obj mapping
+const data = {
+  "firstName":"John",
+  "lastName":"Smith",
+  "amount":500,
+  "date":"2019-07-15",
+  "pic":"PTSTRANSFER"
+};
+
+Handlebars.registerHelper('debitRewards', function(data) {
+  // create a new copy of the original context data
+  const globalRewards = {...data};
+  // build a new obj with mappings to reflect Global Rewards JSON
+  const mappedObj = {
+    fName : globalRewards.data.root.firstName,
+    lName : globalRewards.data.root.lastName,
+    pointsTotal : -Math.abs(globalRewards.data.root.amount),
+    debitTime : Date.now(),
+    activityCode : globalRewards.data.root.pic,
+    description : `Points transferred - ${globalRewards.data.root.date}`
+  };
+  return JSON.stringify(mappedObj);
+});
+
+const debitRewardsHtml = document.getElementById('points').innerHTML;
+const rewardsTemplate = Handlebars.compile(debitRewardsHtml);
+const rewardsHtml = rewardsTemplate(data);
+document.getElementById('points').innerHTML = rewardsHtml;
