d45b3d50e586432d6a995dfc1ced69f171e8775b
surajjayraman <suraj.jayraman@gmail.com>
2021-12-20 15:40:47 -0500

    Create sample data source and use strict mode to test handlebars template.

--- fishman-handlebar.js
+++ fishman-handlebar.js
@@ -1,8 +1,23 @@
 // data
-const name = {name: "Suraj Nair"};
+const fullName = {name: "Suraj Nair"};
 const source = document.getElementById('fishman').innerHTML;
 const template = Handlebars.compile(source);
 
 // compiled html
-const compiledHtml = template(name);
+const compiledHtml = template(fullName);
 document.getElementById('fishman').innerHTML = compiledHtml;
+
+// sample data source
+const data = {
+  company: "Freddy's Fish Farm",
+  phone: '619-555-1212'
+};
+
+// Use strict mode so that Handlebars will throw exceptions if we
+// attempt to use fields in our template that are not in our data set.
+const templateSource = document.getElementById('source').innerHTML;
+const hbTemplate = Handlebars.compile(templateSource, {strict: true});
+const result = hbTemplate(data);
+console.log(result);
+
+document.getElementById('source').innerHTML = result;
